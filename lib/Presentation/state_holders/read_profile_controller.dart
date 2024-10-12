493d3a0a4116eab4100c8a3c09b08df07e84f791
dev-asib <mail.asib.com@gmail.com>
2024-10-12 17:37:03 +0600

    Updated

--- lib/Presentation/state_holders/read_profile_controller.dart
+++ lib/Presentation/state_holders/read_profile_controller.dart
@@ -1,21 +1,18 @@
 import 'package:crafty_bay/Presentation/state_holders/auth_controller.dart';
 import 'package:crafty_bay/data/Utils/urls.dart';
 import 'package:crafty_bay/data/models/network_response.dart';
-import 'package:crafty_bay/data/models/product_details_model.dart';
 import 'package:crafty_bay/data/services/network_caller.dart';
 import 'package:get/get.dart';
 
 class ReadProfileController extends GetxController {
   bool _inProgress = false;
   bool _isProfileCompleted = false;
-  ProductDetailsModel? _productDetailsModel;
   String? _errorMessage;
 
   bool get inProgress => _inProgress;
 
   bool get isProfileCompleted => _isProfileCompleted;
 
-  ProductDetailsModel? get productDetailsModel => _productDetailsModel;
 
   String? get errorMessage => _errorMessage;
 
